efa217a141d2debd8ef79fcc474603e09c24224a
Tuyet Phan <>
2018-12-04 20:39:03 -0800

    refactored recipe controller code

--- api/controllers/RecipeController.js
+++ api/controllers/RecipeController.js
@@ -1,5 +1,4 @@
 const RecipeActionClass = require('../helperss/actionClasses/RecipeActionClass');
-const RecipeFormClass = require('../helperss/formClasses/RecipeFormClass');
 
 module.exports= {
 
@@ -166,17 +165,7 @@ module.exports= {
             return res.notFound();
         }
 
-        await UserProfile.addToCollection(currentUser.userProfile[0].id, 'cookbook').members([recipe.id]);
-
-        let followers = await UserProfile.findOne({
-            id: currentUser.userProfile[0].id
-        });
-
-        await CommunityRecipe.create({
-            userProfile: followers.followerList,
-            recipeId: recipe.id,
-            savedBy: currentUser.id,
-        });
+        await RecipeActionClass.addToCookbook(currentUser, recipe);
 
         recipe.pageName = 'viewRecipe';
 
@@ -210,12 +199,8 @@ module.exports= {
             return res.notFound();
         }
 
-        await CommunityRecipe.destroy({
-            recipeId: recipe.id,
-            savedBy: currentUser.id,
-        });
+        await RecipeActionClass.removeFromCookbook(currentUser, recipe);
 
-        await UserProfile.removeFromCollection(currentUser.userProfile[0].id, 'cookbook').members([recipe.id]);
         recipe.pageName = 'viewRecipe';
 
         return res.redirect('/recipe/' + recipe.id);
@@ -238,24 +223,7 @@ module.exports= {
             return res.notFound();
         }
 
-        await Review.create({
-            user: req.session.userId,
-            rating: rating,
-            reviewBody: review,
-            recipe: recipeId,
-        });
-
-        let recipe = await Recipe.findOne(recipeId);
-        
-        let allReviews = await Review.find({
-            recipe: recipeId,
-        });
-
-        let newRating = recipe.rating == 0 ? rating : ((Number(recipe.rating) * (Number(allReviews.length) - 1)) + Number(rating)) / Number(allReviews.length);
-
-        await Recipe.update(recipeId, {
-            rating: newRating,
-        });
+        await RecipeActionClass.createReview(recipeId, rating, review, req.session.userId);
 
         return res.redirect('/recipe/' + recipeId);
 
--- api/helperss/actionClasses/RecipeActionClass.js
+++ api/helperss/actionClasses/RecipeActionClass.js
@@ -10,4 +10,48 @@ module.exports = {
         });
         return savedRecipe.cookbook.length > 0 ? true : false;
     },
+
+    addToCookbook: async function(currentUser, recipe) {
+        await UserProfile.addToCollection(currentUser.userProfile[0].id, 'cookbook').members([recipe.id]);
+
+        let followers = await UserProfile.findOne({
+            id: currentUser.userProfile[0].id
+        });
+
+        await CommunityRecipe.create({
+            userProfile: followers.followerList,
+            recipeId: recipe.id,
+            savedBy: currentUser.id,
+        });
+    },
+
+    removeFromCookbook: async function(currentUser, recipe) {
+        await CommunityRecipe.destroy({
+            recipeId: recipe.id,
+            savedBy: currentUser.id,
+        });
+
+        await UserProfile.removeFromCollection(currentUser.userProfile[0].id, 'cookbook').members([recipe.id]);
+    },
+
+    createReview: async function(recipeId, rating, review, userId) {
+        await Review.create({
+            user: userId,
+            rating: rating,
+            reviewBody: review,
+            recipe: recipeId,
+        });
+
+        let recipe = await Recipe.findOne(recipeId);
+        
+        let allReviews = await Review.find({
+            recipe: recipeId,
+        });
+
+        let newRating = recipe.rating == 0 ? rating : ((Number(recipe.rating) * (Number(allReviews.length) - 1)) + Number(rating)) / Number(allReviews.length);
+
+        await Recipe.update(recipeId, {
+            rating: newRating,
+        });
+    }
 }
